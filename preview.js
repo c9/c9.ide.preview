b6d2dfe356d49d5f08a8b98060028e151da8a9f4
nightwing <amirjanyan@gmail.com>
2015-11-04 21:15:53 +0000

    fix preview running app on standlone

--- preview.js
+++ preview.js
@@ -192,9 +192,7 @@ define(function(require, exports, module) {
                     }
                     
                     if (args.server) {
-                        var hostname = options.local
-                            ? "localhost:8080"
-                            : c9.hostname;
+                        var hostname = c9.hostname || "localhost:8080";
                         
                         var cb = function(err, stderr, stdout) {
                             if (err && err.code != 1) 
